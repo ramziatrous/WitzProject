af3027ace39c19fba53869230fadd9796381a90d
Ramzi Atrous <atrous.ramzi@gmx.de>
2023-08-23 13:56:50 +0200

    Backend init

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,6 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    ButtonModule,
     AppRoutingModule
   ],
   providers: [],
