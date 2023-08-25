15cda221900d8a547878a90ff4a777d66e1c4c59
Nick Jabs <yabsikus@gmail.com>
2023-08-25 13:03:53 +0100

    RAMZI teste das GET-RANDOM-JOKE API :)

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,9 +3,11 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected 🏖️ { conn.connection.host}");
+    console.log(
+      ` 🚓 Okay you pass --> MongoDB Connected ✴️ ${conn.connection.host}`
+    );
   } catch (err) {
-    console.log("Error: ☠️ ${error.message}");
+    console.error(`Error: ☠️ ${err.message}`);
     process.exit(1);
   }
 };
