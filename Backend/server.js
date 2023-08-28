0638827fe3c0b8b0e16fc5aead4a5982429a34f9
Nick Jabs <yabsikus@gmail.com>
2023-08-28 12:44:18 +0100

    updated server.js CORS Config *

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // CORS-Konfiguration hinzufügen
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header(
     "Access-Control-Allow-Headers",
