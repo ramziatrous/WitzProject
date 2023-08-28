1ebdf23f1560fd138cb82f0333f3e884443e1014
Nick Jabs <yabsikus@gmail.com>
2023-08-28 12:11:38 +0100

    updated server.js CORS Config

--- Backend/routes/apiRoutes.js
+++ Backend/routes/apiRoutes.js
@@ -3,8 +3,8 @@ import * as jokesController from "../controllers/jokesController.js";
 
 const router = express.Router();
 
-router.get("/jokes", jokesController.getAllJokes); // Use getAllJokes function
-router.post("/jokes", jokesController.addNewJoke); // Use addNewJoke function
+router.get("/jokes", jokesController.getAllJokes); // getAllJokes function
+router.post("/jokes", jokesController.addNewJoke); // addNewJoke function
 // router.put("/jokes/:id", jokesController.updateJoke);
 router.get("/jokes/random", jokesController.getRandomJoke);
 
