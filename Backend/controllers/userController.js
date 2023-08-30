3af2ea733f2ccac7f25c66088677a6034a91b928
Nick Jabs <yabsikus@gmail.com>
2023-08-30 17:41:59 +0100

    added LOGIN and REGISTER Routes

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,5 +1,21 @@
 import { User } from "../models/userModel.js";
 import jwt from "jsonwebtoken";
+import multer from "multer";
+import path from "path";
+
+// Configure multer for file uploads
+const storage = multer.diskStorage({
+  destination: "uploads/", // Specify the folder where files will be stored
+  filename: function (req, file, cb) {
+    // Define the file name (you can modify this as needed)
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+const upload = multer({ storage });
 
 //@desc Auth user & get token
 //@route POST users/login
@@ -25,7 +41,7 @@ const authUser = async (req, res) => {
       const token = jwt.sign(payload, "123456");
 
       // Send the token in the response
-      res.status(200).send({ mytoken: token });
+      res.status(200).json({ mytoken: token });
     } else {
       // Return an error response if authentication fails
       res.status(401).json({ message: "Invalid email or password 👨‍🦲" });
@@ -36,23 +52,58 @@ const authUser = async (req, res) => {
   }
 };
 
-//@desc register user
+//@desc register user with picture upload
 //@route POST users/register
 //@access Public
 const registerUser = async (req, res) => {
-  try {
-    res.send(" ✅ Register user response");
-  } catch (error) {
-    res.status(500).json({ message: "Server error" });
-  }
+  const { username, email, password } = req.body;
+
+  // Handle file upload using multer
+  upload.single("image")(req, res, async function (err) {
+    if (err) {
+      return res.status(500).json({ message: "File upload error" });
+    }
+
+    const imageFileName = req.file ? req.file.filename : "default.jpg"; // Get the uploaded file name or set default
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      res.status(400).json({ message: "User already exists" });
+    } else {
+      try {
+        const user = await User.create({
+          username,
+          email,
+          password,
+          image: imageFileName, // Save the image file name in the database
+        });
+
+        if (user) {
+          res.status(201).json({
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            image: user.image,
+          });
+        } else {
+          res.status(400).json({ message: "Invalid user data" });
+        }
+      } catch (error) {
+        res.status(500).json({ message: "Server error" });
+      }
+    }
+  });
 };
 
 //@desc logout user / clear cookie
 //@route POST users/logout
 //@access Private
 const logoutUser = async (req, res) => {
+  // You can add logout functionality here if needed
   try {
-    res.send(" ✅ Logout user response");
+    res.send("✅ I was written in the Front-End!");
   } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -1,38 +1,61 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
+// Definiere das Benutzerschema unter Verwendung von Mongoose
 const userSchema = new mongoose.Schema(
   {
+    // Benutzername des Benutzers
     username: {
       type: String,
       required: true,
     },
+    // E-Mail-Adresse des Benutzers
     email: {
       type: String,
       required: true,
       unique: true,
     },
+    // Passwort des Benutzers (gehasht)
     password: {
       type: String,
       required: true,
     },
+    // Gibt an, ob der Benutzer ein Administrator ist
     isAdmin: {
       type: Boolean,
       required: true,
       default: false,
     },
+    // Profilbild des Benutzers (optional)
     image: {
       type: String,
       required: false,
     },
   },
-  { timestamps: true }
+  { timestamps: true } // Automatische Zeitstempel-Felder (Erstellt am, Aktualisiert am)
 );
 
-userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
+// Methode für das Benutzerschema, um Passwörter zu überprüfen
+userSchema.methods.matchPassword = async function (eingegebenesPasswort) {
+  return await bcrypt.compare(eingegebenesPasswort, this.password);
 };
 
+// Pre-Save-Hook, um das Passwort vor dem Speichern in der Datenbank zu hashen
+userSchema.pre("save", async function (next) {
+  // Das Passwort nur hashen, wenn es geändert wurde (neu oder aktualisiert)
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  // Ein Salz generieren und das Passwort hashen
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hashSync(this.password, salt);
+
+  next();
+});
+
+// Das User-Modell erstellen, basierend auf dem definierten Schema
 const User = mongoose.model("User", userSchema);
 
+// Exportieren des User-Modells
 export { User };
